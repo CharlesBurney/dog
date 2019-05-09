b175449b50bbfbbeedd1a3cc10c67d5bfab1464a
Charles Burney <charlesburney@mwc-104202.dhcp.missouri.edu>
2019-05-09 15:47:24 -0500

    add dog

---
+++ DogProgram/DogProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DogProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
